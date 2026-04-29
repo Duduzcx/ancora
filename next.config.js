db2930384599fbb2c1d902b408f6e208bcdbdb30
Duduzcx <eduardosantosbbezerra@gmail.com>
2026-04-29 20:44:29 -0300

    fix: netlify functions and hybrid build support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: process.env.IS_CAPACITOR_BUILD === 'true' ? 'export' : undefined,
   images: {
     unoptimized: true,
   },
